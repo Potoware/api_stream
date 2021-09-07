06bcff383ed954aa57013a75310ba064918f2008
Alejandro Potosi <alejandropotosi@outlook.com>
2021-09-07 09:02:35 -0500

    API Stream, Map con opciones adicionales

--- src/com/potoware/api/stream/ejemplos/EjemploStreamMap.java
+++ src/com/potoware/api/stream/ejemplos/EjemploStreamMap.java
@@ -1,42 +1,48 @@
 package com.potoware.api.stream.ejemplos;
 
+import com.potoware.api.stream.ejemplos.models.Usuario;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class EjemploStream {
+public class EjemploStreamMap {
 
     public static void main(String[] args) {
 
-        System.out.println("\nStream of directo  a Stream");
-        Stream<String> nombres = Stream.of("Pato","Paco", "Pepe","Pepa");
+
+        System.out.println("\n--------MAP---------");
+        Stream<String> nombres = Stream.of("Pato","Paco", "Pepe","Pepa").map(nombre -> {
+            return nombre.toUpperCase();
+        });
+
         nombres.forEach(System.out::println);
 
-        System.out.println("\nArreglo a Stream");
-        String[] arr = {"Pato","Paco", "Pepe","Pepa"};
-        Stream<String> nombres2 = Arrays.stream(arr);
-        nombres2.forEach(System.out::println);
+        System.out.println("\n--------peak---------");
+        Stream<String> nombres2 = Stream.of("Pato","Paco", "Pepe","Pepa").peek(e-> System.out.println(e)).map(nombre -> {
+            return nombre.toUpperCase();
+        });
+        List<String> lista = nombres2.collect(Collectors.toList());
+        System.out.println("\n--------a lista---------");
+        for (String nom: lista) {
+            System.out.println(nom);
+        }
+        //nombres2.forEach(System.out::println);
 
-        System.out.println("\nString Builder a Stream");
-        Stream<String> nombres3 = Stream.<String>builder().add("Apco")
-                .add("Pico")
-                .add("Poco")
-                .add("Puca")
-                .add("Paca")
-                .build();
+        System.out.println("\n--------creando usuarios desde lista---------");
+        Stream<Usuario> usuarios = Stream
+                .of("Pato Socio","Paco Social", "Pepe Sociedad","Pepa Sucio")
+                .map(nombre -> new Usuario(nombre.split(" ")[0],nombre.split(" ")[1]))
+                .peek(u-> System.out.println(u.getNombres()))
+                .map(usuario -> {
+                    String nombre = usuario.getNombres().toUpperCase();
+                    usuario.setNombres(nombre);
+                    return usuario;
+                });
+      List<Usuario>usuariosList = usuarios.collect(Collectors.toList());
 
-        nombres3.forEach(System.out::println);
-
-        System.out.println("\nLista a Stream");
-        List<String> lista = new ArrayList<>();
-        lista.add("Paco");
-        lista.add("Peco");
-        lista.add("Pico");
-        lista.add("Poco");
-        lista.add("Puca");
-        Stream<String> nombres4 = lista.stream();
-        nombres4.forEach(System.out::println);
-
+        usuariosList.forEach(u-> System.out.println(u.getApellidos()));
     }
 }
---
+++ src/com/potoware/api/stream/ejemplos/models/Usuario.java
@@ -0,0 +1,32 @@
+package com.potoware.api.stream.ejemplos.models;
+
+public class Usuario {
+    private String nombres;
+    private String apellidos;
+
+    public Usuario(String nombres, String apellidos) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    @Override
+    public String toString() {
+        return nombres +" " + apellidos ;
+    }
+}
